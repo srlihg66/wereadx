346aa1e980580967647e6bc802615a32a7d883fc
srlihg66 <64582113+srlihg66@users.noreply.github.com>
2024-07-24 17:18:22 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
-export const UserAgentForWeb = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36";
+export const UserAgentForWeb = "Mozilla/5.0 (iPhone; CPU iPhone OS 16_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/126.0.6478.153 Mobile/15E148 Safari/604.1";
 
-export const UserAgentForApp = "WeRead/7.4.2 (iPhone; iOS 17.1; Scale/3.00)"
+export const UserAgentForApp = "WeRead/8.2.2 (iPhone; iOS 16.6.1; Scale/3.00)"
 
 // 每月下载限额
-export const MAX_DOWNLOAD_COUNT_PER_MONTH = 30;
+export const MAX_DOWNLOAD_COUNT_PER_MONTH = 1000;
